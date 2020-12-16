50da42a76c3ddc4302d7c7f228d1d71a0ca04930
guilherme Pires <guilhermecspires@gmail.com>
2020-12-16 13:16:01 +0000

    login, register e forgot password
    falta Placeholders, e completar o home(dashboard)

--- roboRaul/resources/views/auth/login.blade.php
+++ roboRaul/resources/views/auth/login.blade.php
@@ -27,22 +27,40 @@
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="/index3.html" method="post">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
                             </div>
                         </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password" required
+                            autocomplete="current-password">
+
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                     <div class="row">
                         <div class="col-8">
@@ -60,6 +78,7 @@
                         <!-- /.col -->
                     </div>
                 </form>
+                {{--
 
                 <div class="social-auth-links text-center mb-3">
                     <p>- OR -</p>
@@ -72,11 +91,16 @@
                 </div>
                 <!-- /.social-auth-links -->
 
+                --}}
                 <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
+                    @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}">
+                        {{ __('Esqueci a senha') }}
+                    </a>
+                    @endif
                 </p>
                 <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
+                    <a href="/register" class="text-center">Novo Utilizador</a>
                 </p>
             </div>
             <!-- /.login-card-body -->
